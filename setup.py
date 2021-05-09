9ecdb5fbeb5a30511ee6630ce2b44f378b27fd76
ChanceNCounter <ChanceNCounter@icloud.com>
2021-05-08 19:53:43 -0700

    bump version: 0.4.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("readme.md", "r") as fh:
 
 setup(
     name='lingua_franca',
-    version='0.4.1',
+    version='0.4.2',
     packages=['lingua_franca', 'lingua_franca.lang'],
     url='https://github.com/MycroftAI/lingua-franca',
     license='Apache2.0',
