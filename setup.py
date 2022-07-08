9fd96adfeae3e5994ce4e2629f38b64e6f760489
Kris Gesling <kris.gesling@mycroft.ai>
2022-07-08 16:22:56 +0930

    Remove Python 3.6 support as it has reached EOL

--- setup.py
+++ setup.py
@@ -45,7 +45,6 @@ setup(
         'Topic :: Text Processing :: Linguistic',
         'License :: OSI Approved :: Apache Software License',
 
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
