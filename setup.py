fcbb1e1531120eec3b190528d90d4d977a9d417d
Kris Gesling <kris.gesling@mycroft.ai>
2022-07-08 16:23:41 +0930

    Bump release for Basque support

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("readme.md", "r") as fh:
 
 setup(
     name='lingua_franca',
-    version='0.4.2',
+    version='0.4.3',
     packages=['lingua_franca', 'lingua_franca.lang'],
     url='https://github.com/MycroftAI/lingua-franca',
     license='Apache2.0',
